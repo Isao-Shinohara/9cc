a7cae5381c216448ef935ea00a016f9a88e4016f
Isao-Shinohara <isao.shinohara.7@gmail.com>
2018-12-03 20:02:56 +0900

    整数1つをコンパイルするコンパイラを作成

--- test.sh
+++ test.sh
@@ -8,8 +8,10 @@ try(){
   ./tmp
   actual="$?"
 
-  if [ "$actual" != "$expected" ]; then
-    echo "$input exected, but got $actual"
+  if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+  else
+    echo "$expected exected, but got $actual"
     exit 1
   fi
 }
